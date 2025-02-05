6a7761af009056422e5a8f287f69c0ee8ffca9a5
José Arthur <102533778+JoseArthurAlmeida@users.noreply.github.com>
2025-02-05 16:18:37 +0000

    Adição do código de da sequência de fibonacci NÃO recursivo

--- codigos/bloco 3/03-02-2025/fibonacci-recursivo.py
+++ codigos/bloco 3/03-02-2025/fibonacci-recursivo.py
@@ -1,8 +1,17 @@
-def fibonacci(n):
+def fibonacci(quantidade):
+    fibonacci = [0, 1]
+
+    for i in range(2, quantidade):
+        soma = fibonacci[i - 1] + fibonacci[i - 2]
+        fibonacci.append(soma)
+    
+    return fibonacci[quantidade - 1]
+
+def fibonacciRecursivo(n):
     if n <= 1:
         return n
     return fibonacci(n - 1) + fibonacci(n - 2) 
 
-print(fibonacci(5)) # 5
-print(fibonacci(6)) # 8
-print(fibonacci(2)) # 1
+print(fibonacci(45)) # 5
+# print(fibonacci(6)) # 8
+# print(fibonacci(2)) # 1
