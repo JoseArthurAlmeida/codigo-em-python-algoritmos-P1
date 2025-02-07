54a0966145de69520505eea64f7ae89f71081654
José Arthur <ja1364091@gmail.com>
2025-02-07 09:24:50 -0300

    Correção na função de soma dos dígitos

--- codigos/bloco 3/03-02-2025/soma-dos-digitos-recursivo.py
+++ codigos/bloco 3/03-02-2025/soma-dos-digitos-recursivo.py
@@ -1,7 +1,7 @@
 def somaDigitos(numero):
     if numero < 10:
         return numero
-    return somaDigitos(numero // 10) + somaDigitos(numero % 10)
+    return somaDigitos(numero // 10) + numero % 10
 
 print(somaDigitos(3)) # 3
 print(somaDigitos(30)) # 3
